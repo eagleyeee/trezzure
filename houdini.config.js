f6b0386db173a409507915793655390153cad7bc
Aqeel Chishti <aqeel.chishti@outlook.com>
2025-01-16 16:38:30 +0500

    Update 2

--- houdini.config.js
+++ houdini.config.js
@@ -1,13 +1,21 @@
+import { SALEOR_API_URL, SALEOR_API_TOKEN } from '$env/static/public';
+
 /// <references types="houdini-svelte">
 
 /** @type {import('houdini').ConfigFile} */
 const config = {
+	schemaPath: './schema.graphql', // local schema file
+	sourceGlob: 'src/**/*.{svelte,ts,js}', // source files
 	watchSchema: {
-		url: 'https://trezzure.eu.saleor.cloud/graphql/'
+		url: SALEOR_API_URL
 	},
+	apiUrl: SALEOR_API_URL,
 	runtimeDir: '.houdini',
 	plugins: {
 		'houdini-svelte': {}
+	},
+	headers: {
+		Authorization: `Bearer ${SALEOR_API_TOKEN}`
 	}
 };
 
--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,7 @@
 import { HoudiniClient } from '$houdini';
 
 export default new HoudiniClient({
-    url: 'https://store-ie6rybnk.saleor.cloud/graphql/'
+    url: 'https://trezzure.eu.saleor.cloud/graphql/'
 
     // uncomment this to configure the network call (for things like authentication)
     // for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
